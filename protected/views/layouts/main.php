f0d204546d6b255797dadc9b0922e4bccdd8ff5a
Moisés Aguilar <monchito007@gmail.com>
2014-09-08 23:26:24 +0200

    shortuc icon

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -11,7 +11,7 @@
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
 	<![endif]-->
-
+        <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
         <!-- CSS DatePicker -->
